aad173cde4ac74e59d7007b6c6372d49c5b852fc
Lia <alievaaliyah@gmail.com>
2025-07-07 19:30:41 +0600

    Add lombok support

--- src/main/java/com/lia/pjd/model/InterviewQuestionEntity.java
+++ src/main/java/com/lia/pjd/model/InterviewQuestionEntity.java
@@ -1,8 +1,10 @@
 package com.lia.pjd.model;
 
+import lombok.Data;
+
 import java.util.Objects;
 
-
+@Data
 public class InterviewQuestionEntity {
     private Long id;
     private String question;
@@ -20,39 +22,5 @@ public class InterviewQuestionEntity {
         this.id = null;
     }
 
-    public String getQuestion() {
-        return question;
-    }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        InterviewQuestionEntity that = (InterviewQuestionEntity) o;
-        return Objects.equals(id, that.id) && Objects.equals(question, that.question) && Objects.equals(answer, that.answer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, question, answer);
-    }
 }
